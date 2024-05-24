aa5ac1ae5f01bd93af2895f8e89273eab7c73807
kntmicheal <31380670+kntmicheal@users.noreply.github.com>
2024-05-24 08:28:59 -0700

    well completed assignment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+name = input("Please enter your name: ")# Ask the user for name"
+
+age = input("Please enter your age: ")# Ask the user for his or her "age"
+
+location = input("Please enter your location: ")# Ask the user for his /her location"
+
+print("Hello "+ name + ", you are  "+ age + " years old and live in "+ location)
